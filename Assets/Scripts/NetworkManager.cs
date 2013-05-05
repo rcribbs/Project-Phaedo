d3802178e7a28ffb25466aa9138cf938d33d7517
Justin Ng <mr.justinng@gmail.com>
2013-05-05 15:26:39 -0700

    - basic network spawning, INCOMPLETE

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -6,12 +6,15 @@ public class NetworkManager : MonoBehaviour {
 	public bool isServer = true;
 	public bool isConnectingToServer = false;
 	public string ipAddress = "127.0.0.1";
+	public GameObject playerPrefab;
+	
+	int playerCount = 0;
 
 	// Use this for initialization
 	void Start ()
 	{
 		if(isServer)
-			Network.InitializeServer( 2, 6758 );
+			Network.InitializeServer( 10, 6758, false );
 		else if(isConnectingToServer)
 			ConnectToServer();
 	}
@@ -25,4 +28,24 @@ public class NetworkManager : MonoBehaviour {
 	{
 		Network.Connect( ipAddress, 6758, "" );
 	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log ( "Connected to server." );
+		
+		
+	}
+	
+	void OnPlayerConnected(NetworkPlayer player)
+	{
+		Debug.Log( "Player connected from: " + player.externalIP );
+		playerCount++;
+		
+		GameObject playerObject = (GameObject) Network.Instantiate( playerPrefab, new Vector3(82, 28, 294), Quaternion.identity, 0 );
+		
+		NetworkView networkView = playerObject.GetComponent<NetworkView>();
+		networkView.RPC( "SetOwner", RPCMode.AllBuffered, player );
+	}
+	
+	
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 
 using Damageable;
 
-[RequireComponent( typeof(Rigidbody) )]
+[RequireComponent( typeof( Rigidbody ) )]
+[RequireComponent( typeof( NetworkView ) )]
 
 public class Player : MonoBehaviour
 {
-    
+	public GameObject playerCameraPrefab;
+	
+	private bool controlEnabled;
+	
     public TextAsset damagableData;
-    public float WalkSpeed = 8;
-    public float MidAirSpeed = 8;
-    public float JumpStrength = 280;
-    public float JumpingSurfaceAngleStrictness = 0.7f;  //between 0 to 1; higher value requires flatter surface
+	public float WalkSpeed = 8;
+	public float MidAirSpeed = 8;
+	public float JumpStrength = 280;
+	public float JumpingSurfaceAngleStrictness = 0.7f;  //between 0 to 1; higher value requires flatter surface
  
     bool m_TouchingGround = true;
     private float m_horizontalRotation = 0;
@@ -22,6 +26,8 @@ public class Player : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
+		controlEnabled = false;
+    	
         Terrain theMap = GameObject.FindGameObjectWithTag ("Map").GetComponent<Terrain> ();
         transform.position = new Vector3 (transform.position.x, theMap.SampleHeight (transform.position) + 10, transform.position.z);
         
@@ -39,6 +45,11 @@ public class Player : MonoBehaviour
  
     void FixedUpdate ()
     {
+		if ( !controlEnabled )
+		{
+			return;
+		}
+		
         /* Lateral movement */
         if (m_TouchingGround)
         {
@@ -94,22 +105,22 @@ public class Player : MonoBehaviour
     }
  
 
-    /// <summary>
-    /// Rotates the player with the camera.
-    /// </summary>
-    /// <param name='horizontalRotation'>
-    /// Horizontal rotation of the player.
-    /// </param>
-    public void rotatePlayer (float horizontalRotation)
-    {
-        m_horizontalRotation = horizontalRotation;
-        transform.rotation = Quaternion.Euler (transform.forward + new Vector3 (0, m_horizontalRotation, 0));
-    }
-    /// <summary>
-    /// Instantaneous velocity change, a la CharacterController movement
-    /// </summary>
-    private void MoveOnGround ()
-    {
+	/// <summary>
+	/// Rotates the player with the camera.
+	/// </summary>
+	/// <param name='horizontalRotation'>
+	/// Horizontal rotation of the player.
+	/// </param>
+	public void rotatePlayer(float horizontalRotation)
+	{
+		m_horizontalRotation = horizontalRotation;
+		transform.rotation = Quaternion.Euler( transform.forward + new Vector3( 0, m_horizontalRotation, 0) );
+	}
+	/// <summary>
+	/// Instantaneous velocity change, a la CharacterController movement
+	/// </summary>
+	private void MoveOnGround()
+	{
 //------------------- Rohan: Modified your code a little to work better with rotation. -------------------
         // Only apply impulses if we're actually pressing the key.
         if ((Input.GetAxis ("Vertical") != 0) || (Input.GetAxis ("Horizontal") != 0))
@@ -131,23 +142,51 @@ public class Player : MonoBehaviour
             isWalking = false;
         }
 //------------------- Rohan: This is where my modifications end. --------------------------------------------
-     
-        /* Rohan: I've preserved your code down here.
-     Vector3 targetVelocity = new Vector3( Input.GetAxisRaw( "Horizontal" ) * WalkSpeed - rigidbody.velocity.x,
-                                     0,
-                                     Input.GetAxisRaw( "Vertical" ) * WalkSpeed - rigidbody.velocity.z );
-     rigidbody.AddForce( targetVelocity , ForceMode.VelocityChange );
-     */
-    }
- 
-    /// <summary>
-    /// Gradual velocity change.
-    /// </summary>
-    private void MoveInAir ()
-    {
-        Vector3 velocityChange = new Vector3 (Input.GetAxisRaw ("Horizontal") * MidAirSpeed,
-                                             0,
-                                             Input.GetAxisRaw ("Vertical") * MidAirSpeed);
-        rigidbody.AddForce (velocityChange);
-    }
+		
+		/* Rohan: I've preserved your code down here.
+		Vector3 targetVelocity = new Vector3( Input.GetAxisRaw( "Horizontal" ) * WalkSpeed - rigidbody.velocity.x,
+										0,
+										Input.GetAxisRaw( "Vertical" ) * WalkSpeed - rigidbody.velocity.z );
+		rigidbody.AddForce( targetVelocity , ForceMode.VelocityChange );
+		*/
+	}
+	
+	/// <summary>
+	/// Gradual velocity change.
+	/// </summary>
+	private void MoveInAir()
+	{
+		Vector3 velocityChange = new Vector3( Input.GetAxisRaw( "Horizontal" ) * MidAirSpeed,
+												0,
+												Input.GetAxisRaw( "Vertical" ) * MidAirSpeed );
+		rigidbody.AddForce( velocityChange );
+	}
+	
+	void OnNetworkInstantiate(NetworkMessageInfo networkMessageInfo)
+	{
+		
+	}
+	
+	[RPC]
+	void SetOwner(NetworkPlayer networkPlayer)
+	{
+		Debug.Log ("setting owner");
+		
+		if ( networkPlayer == Network.player )
+		{
+			Debug.Log ("Owner set to me");
+			
+			//Player playerScript = player.GetComponent<Player>();
+			
+			AttachPlayerCamera();
+		}
+	}
+	
+	void AttachPlayerCamera()
+	{
+		GameObject playerCamera = (GameObject) Instantiate(playerCameraPrefab);
+		PlayerCamera playerCameraScript = playerCamera.GetComponent<PlayerCamera>();
+		playerCameraScript.SetTarget(this.gameObject);
+		playerCamera.camera.enabled = true;
+	}
 }
